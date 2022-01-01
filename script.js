b3ac418de5dfd5ccf0c91d111c7fe3ea5706b386
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2022-01-01 18:54:40 +0000

    Add constants

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
-	.then(response => response.json())
-	.then(data => {
-		console.log(data.data);
-	})
+const URL = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+
+let request = new XMLHttpRequest();
+
+const WIDTH = 800;
+const HEIGHT = 500;
+const PADDING = 40;
+
