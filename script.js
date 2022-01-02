5b0b4dbea1bd2685a7688f1d46feefaefea1679b
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2022-01-02 18:17:44 +0000

    Invert svg elements

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // const URL = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
-const WIDTH = 800;
-const HEIGHT = 500;
+const WIDTH = 600;
+const HEIGHT = 400;
 const PADDING = 40;
+
+const M = 3; // constant that scales the data points.
 
 const dataset = [12, 31, 22, 17, 25, 18, 29, 14];
 
@@ -17,9 +19,11 @@ svg.selectAll("rect")
    .enter()
    .append("rect")
    .attr("width", 25) // rect width
-   .attr("height", d => d * 3) // rect height (set bar height to data value - multiplied by 3)
+   .attr("height", d => d * M) // rect height (set bar height to data value - multiplied by data scale constants)
    .attr("x", (d, i) => i * 30) // set each bar coordinates (using the width + 5 for space)
-   .attr("y", 0);
+   .attr("y", (d, i) => {
+       return HEIGHT - M * d;
+   }); // Inverting svg element by accounting for both the height of the bar and the total height of the SVG area
               
 
 
