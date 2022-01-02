4a6d86cf9275469d2989c561856719010305b941
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2022-01-02 19:03:52 +0000

    Add toolip

--- script.js
+++ script.js
@@ -22,14 +22,19 @@ svg.selectAll("rect")
    .attr("height", d => d * M) // rect height (set bar height to data value - multiplied by data scale constants)
    .attr("x", (d, i) => i * 30) // set each bar coordinates (using the width + 5 for space)
    .attr("y", d => HEIGHT - M * d) // Inverting svg element by accounting for both the height of the bar and the total height of the SVG area
-   .attr("fill", "blue"); // set the color of the bars
-  
+   .attr("fill", "blue") // set the color of the bars
+   .attr("class", "bar")  // add  a bar class to rect
+   .append("title") // add toolip element
+   .text(d => d) // toolip data
+
+
+
 // Labels Setup      
 svg.selectAll("text")
    .data(dataset)
    .enter()
    .append("text")
-   .text((d) => d)
+   .text(d => d)
    .attr("x", (d, i) => i * 30) // same as the rect
    .attr("y", d => HEIGHT - (d * 3) - M)
    .attr("font-size", 20 + "px");
