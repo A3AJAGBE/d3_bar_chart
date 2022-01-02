869eccdba58a44f8b358b547b2f2a908e538f618
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2022-01-02 17:55:25 +0000

    Add rect shape

--- script.js
+++ script.js
@@ -8,10 +8,15 @@ const dataset = [12, 31, 22, 17, 25, 18, 29, 14];
 
 const svg = d3.select("section")
               .append("svg")
-              .attr("width", WIDTH)
-              .attr("height", HEIGHT)
+              .attr("width", WIDTH) // svg width
+              .attr("height", HEIGHT) // svg height
+              .append("rect")
+              .attr("width", 25) // rect width
+              .attr("height", 100) // rect height
+              .attr("x", 0)
+              .attr("y", 0)
 
-              
+
 // d3.select("section").selectAll("h2")
 //   .data(dataset)
 //   .enter()
